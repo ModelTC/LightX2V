080b1cee200c4e36db5101ad88b4c0f964368657
chendingyu <chendingyu1@sensetime.com>
2025-11-03 17:48:58 +0800

    u

--- lightx2v/__init__.py
+++ lightx2v/__init__.py
@@ -1,6 +1,5 @@
 __version__ = "0.1.0"
 __author__ = "LightX2V Contributors"
-__email__ = ""
 __license__ = "Apache 2.0"
 
 from lightx2v import models, common, deploy, utils
@@ -8,7 +7,6 @@ from lightx2v import models, common, deploy, utils
 __all__ = [
     "__version__",
     "__author__",
-    "__email__",
     "__license__",
     "models",
     "common",
