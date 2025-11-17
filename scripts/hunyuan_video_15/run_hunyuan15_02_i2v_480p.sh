7430fc216270fbbb685de4e6301233f83563b1c1
helloyongyang <yongyang1030@163.com>
2025-11-17 09:17:09 +0000

    Update scripts

--- scripts/hunyuan_video_15/run_hunyuan15_02_i2v_480p.sh
+++ scripts/hunyuan_video_15/run_hunyuan15_02_i2v_480p.sh
@@ -13,7 +13,7 @@ python -m lightx2v.infer \
 --model_cls hunyuan_video_1.5 \
 --task i2v \
 --model_path $model_path \
---config_json ${lightx2v_path}/configs/hunyuan_video_15/hunyuan_video_15_i2v.json \
+--config_json ${lightx2v_path}/configs/hunyuan_video_15/hunyuan_video_15_02_i2v_480p.json \
 --prompt "镜头跟随猪大哥和猪二哥的背影，他们仓皇地从木屋废墟中向远处的砖屋拼命奔跑。木屋废墟的碎片在地面上微微颤动，砖屋在远处显得越来越近。" \
 --negative_prompt "" \
 --image_path /data/nvme1/yongyang/hy15_demo/0.jpg \
