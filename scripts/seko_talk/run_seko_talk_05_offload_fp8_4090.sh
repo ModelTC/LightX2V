cb6e9c167973d667f9408325a693d2cabbb1b9d4
gushiqiao <975033167@qq.com>
2025-11-21 03:15:36 +0000

    fix bug and update hy config

--- scripts/seko_talk/run_seko_talk_05_offload_fp8_4090.sh
+++ scripts/seko_talk/run_seko_talk_05_offload_fp8_4090.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lightx2v_path=/data/nvme0/gushiqiao/hy_latest/LightX2V-HY
-model_path=/data/nvme0/models/SekoTalk-Distill
+lightx2v_path=/path/to/LightX2V
+model_path=/path/to/SekoTalk-Distill-fp8/
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -15,7 +15,7 @@ python -m lightx2v.infer \
 --model_cls seko_talk \
 --task s2v \
 --model_path $model_path \
---config_json /data/nvme0/gushiqiao/hy_latest/LightX2V-HY/configs/seko_talk/5090/seko_talk_5090_bf16.json \
+--config_json ${lightx2v_path}/configs/seko_talk/seko_talk_05_offload_fp8_4090.json \
 --prompt  "The video features a male speaking to the camera with arms spread out, a slightly furrowed brow, and a focused gaze." \
 --negative_prompt 色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走 \
 --image_path ${lightx2v_path}/assets/inputs/audio/seko_input.png \
