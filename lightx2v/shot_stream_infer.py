6efa5b9e42331be842d1a54af0f4d39a51546004
wangshankun <wangshankun2011@hotmail.com>
2025-12-12 06:26:12 +0000

    add offload

--- lightx2v/shot_stream_infer.py
+++ lightx2v/shot_stream_infer.py
@@ -7,28 +7,22 @@ os.environ["SENSITIVE_LAYER_DTYPE"] = "None"
 os.environ["PROFILING_DEBUG_LEVEL"] = "2"
 
 import json
-
-import torch
-import numpy as np
-import torch.distributed as dist
-from loguru import logger
 import subprocess
-from einops import rearrange
+from argparse import Namespace
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union
 
-from lightx2v.models.runners.wan.wan_audio_runner import Wan22AudioRunner, WanAudioRunner  # noqa: F401: F401
+import numpy as np
+import torch
+import torchaudio as ta
+from einops import rearrange
+from loguru import logger
+
 from lightx2v.models.runners.wan.wan_runner import Wan22MoeRunner, WanRunner  # noqa: F401
 from lightx2v.utils.input_info import set_input_info
 from lightx2v.utils.registry_factory import RUNNER_REGISTER
-from lightx2v.utils.set_config import print_config, set_config, set_parallel_config
-from lightx2v.utils.utils import seed_all
-from argparse import Namespace
+from lightx2v.utils.set_config import print_config, set_config
 
-from dataclasses import dataclass
-
-
-import warnings
-from typing import Optional, Tuple, Union
-import torchaudio as ta
 
 def array_to_video(
     image_array: np.ndarray,
@@ -195,11 +189,11 @@ def save_audio(
     return out_video
 
 
-
 @dataclass
 class ClipConfig:
     name: str
-    config_json: str 
+    config_json: str
+
 
 @dataclass
 class ShotConfig:
@@ -225,16 +219,16 @@ class ShotStreamPipeline:
             self.clip_generators[name] = self.create_clip_generator(clip_config)
 
             args = Namespace(
-                seed = self.shot_cfg.seed,
-                prompt = self.shot_cfg.prompt,
-                negative_prompt = self.shot_cfg.negative_prompt,
-                image_path = self.shot_cfg.image_path,
-                audio_path = self.shot_cfg.audio_path,
-                save_result_path = self.shot_cfg.save_result_path,
-                task = self.clip_generators[name].task,
-                return_result_tensor = True,
-                overlap_frame  = self.overlap_frame,
-                overlap_latent = self.overlap_latent,
+                seed=self.shot_cfg.seed,
+                prompt=self.shot_cfg.prompt,
+                negative_prompt=self.shot_cfg.negative_prompt,
+                image_path=self.shot_cfg.image_path,
+                audio_path=self.shot_cfg.audio_path,
+                save_result_path=self.shot_cfg.save_result_path,
+                task=self.clip_generators[name].task,
+                return_result_tensor=True,
+                overlap_frame=self.overlap_frame,
+                overlap_latent=self.overlap_latent,
             )
             self.clip_inputs[name] = set_input_info(args)
 
@@ -246,11 +240,11 @@ class ShotStreamPipeline:
 
     def create_clip_generator(self, clip_config: ClipConfig):
         config = self.get_config_json(clip_config.config_json)
-        #自动计算clip视频帧长度
-        clip_duration     = config.get("video_duration", 2)
+        # 自动计算clip视频帧长度
+        clip_duration = config.get("video_duration", 2)
         overlap_frame_len = config.get("prev_frame_length", 1)
-        target_fps        = config.get("target_fps", 16)
-        config["target_video_length"] =  target_fps * clip_duration + overlap_frame_len
+        target_fps = config.get("target_fps", 16)
+        config["target_video_length"] = target_fps * clip_duration + overlap_frame_len
         config = set_config(Namespace(**config))
         print_config(config)
 
@@ -264,40 +258,39 @@ class ShotStreamPipeline:
             config = json.load(f)
         return config
 
-
     @torch.no_grad()
     def generate(self):
-        s2v = self.clip_generators['s2v_clip']#s2v一致性强，动态相应差
-        f2v = self.clip_generators['f2v_clip']#f2v一致性差，动态响应强
-        #根据 pipe 最长 overlap_len 初始化 tail buffer
-        self.max_tail_len       = max(s2v.prev_frame_length, f2v.prev_frame_length)
-        self.global_tail_video  = None
+        s2v = self.clip_generators["s2v_clip"]  # s2v一致性强，动态相应差
+        f2v = self.clip_generators["f2v_clip"]  # f2v一致性差，动态响应强
+        # 根据 pipe 最长 overlap_len 初始化 tail buffer
+        self.max_tail_len = max(s2v.prev_frame_length, f2v.prev_frame_length)
+        self.global_tail_video = None
 
-        #获取音频路径信息
-        base_audio = self.shot_cfg.audio_path          # "/assets/inputs/audio/seko_input.mp3"
-        audio_dir = os.path.dirname(base_audio)        # "/assets/inputs/audio"
-        audio_prefix = os.path.splitext(os.path.basename(base_audio))[0]   # "seko_input"
-        audio_ext = os.path.splitext(base_audio)[1]    # ".mp3"
+        # 获取音频路径信息
+        base_audio = self.shot_cfg.audio_path  # "/assets/inputs/audio/seko_input.mp3"
+        audio_dir = os.path.dirname(base_audio)  # "/assets/inputs/audio"
+        audio_prefix = os.path.splitext(os.path.basename(base_audio))[0]  # "seko_input"
+        audio_ext = os.path.splitext(base_audio)[1]  # ".mp3"
 
         gen_video_list = []
         cut_audio_list = []
 
-        #Demo 交替生成 clip
+        # Demo 交替生成 clip
         for i in range(7):
             # 选择当前 pipe
             if i % 2 == 0:
                 pipe = s2v
-                inputs = self.clip_inputs['s2v_clip']
+                inputs = self.clip_inputs["s2v_clip"]
             else:
                 pipe = f2v
-                inputs = self.clip_inputs['f2v_clip']
-                inputs.prompt = "A man speaks to the camera with a slightly furrowed brow and focused gaze. He raises both hands upward in powerful, emphatic gestures. " # 添加动作提示
+                inputs = self.clip_inputs["f2v_clip"]
+                inputs.prompt = "A man speaks to the camera with a slightly furrowed brow and focused gaze. He raises both hands upward in powerful, emphatic gestures. "  # 添加动作提示
 
-            inputs.seed       = self.shot_cfg.seed + i  # 不同 clip 使用不同随机种子
-            audio_name        = f"{audio_prefix}_{i+1}{audio_ext}"  # seko_input_0.mp3
+            inputs.seed = self.shot_cfg.seed + i  # 不同 clip 使用不同随机种子
+            audio_name = f"{audio_prefix}_{i + 1}{audio_ext}"  # seko_input_0.mp3
             inputs.audio_path = os.path.join(audio_dir, audio_name)
 
-            if self.global_tail_video is not None:#根据当前 pipe 需要多少 overlap_len 来裁剪 tail
+            if self.global_tail_video is not None:  # 根据当前 pipe 需要多少 overlap_len 来裁剪 tail
                 needed = pipe.prev_frame_length
                 inputs.overlap_frame = self.global_tail_video[:, :, -needed:]
 
@@ -308,15 +301,14 @@ class ShotStreamPipeline:
             gen_video_list.append(gen_clip_video[:, :, :aligned_len])
             cut_audio_list.append(audio_clip)
 
-            self.global_tail_video = gen_clip_video[:, :, -self.max_tail_len:]
-
+            self.global_tail_video = gen_clip_video[:, :, -self.max_tail_len :]
 
         gen_lvideo = torch.cat(gen_video_list, dim=2).float()
         merge_audio = np.concatenate(cut_audio_list, axis=0).astype(np.float32)
         out_path = os.path.join("./", "video_merge.mp4")
         audio_file = os.path.join("./", "audio_merge.wav")
-        
+
         save_to_video(gen_lvideo, out_path, 16)
-        save_audio(merge_audio, audio_file, out_path, output_path="/home/wangshankun/code/LightX2V/save_results/output_lightx2v_seko_talk.mp4")
+        save_audio(merge_audio, audio_file, out_path, output_path="./output_lightx2v_seko_talk.mp4")
         os.remove(out_path)
         os.remove(audio_file)
--- wan_sekotalk.py
+++ wan_sekotalk.py
@@ -1,8 +1,10 @@
 """
 Shot Stream Inference Pipeline for SekoTalk Model
 """
+
 import os
-#设置环境变量
+
+# 设置环境变量
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 os.environ["DTYPE"] = "BF16"
@@ -10,12 +12,12 @@ os.environ["SENSITIVE_LAYER_DTYPE"] = "None"
 os.environ["PROFILING_DEBUG_LEVEL"] = "2"
 
 
-from lightx2v.shot_stream_infer import ShotStreamPipeline, ShotConfig, ClipConfig
+from lightx2v.shot_stream_infer import ClipConfig, ShotConfig, ShotStreamPipeline
 
 seed = 42
 prompt = "The video features a male speaking to the camera with arms spread out, a slightly furrowed brow, and a focused gaze."
 negative_prompt = "镜头晃动，色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
-lightx2v_path = "/home/wangshankun/code/tmp/LightX2V/"
+lightx2v_path = "/mnt/afs1/wangshankun/LightX2V/"
 image_path = lightx2v_path + "/assets/inputs/audio/seko_input.png"
 save_result_path = lightx2v_path + "/save_results/output_lightx2v_seko_talk.mp4"
 audio_path = lightx2v_path + "/assets/inputs/audio/seko_input.mp3"
