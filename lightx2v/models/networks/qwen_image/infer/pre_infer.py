056d0fdbd4ed3d89e036eaa6a60523bdc76fa43f
helloyongyang <yongyang1030@163.com>
2025-12-14 18:01:03 +0000

    update

--- lightx2v/models/networks/qwen_image/infer/pre_infer.py
+++ lightx2v/models/networks/qwen_image/infer/pre_infer.py
@@ -8,22 +8,18 @@ class QwenImagePreInfer:
         self.config = config
         self.attention_kwargs = {}
         self.cpu_offload = config.get("cpu_offload", False)
-        self.zero_cond_t = config.get("zero_cond_t", False)
 
     def set_scheduler(self, scheduler):
         self.scheduler = scheduler
 
     def infer(self, weights, hidden_states, encoder_hidden_states):
-        hidden_states = hidden_states.squeeze(0)
-        hidden_states = weights.img_in.apply(hidden_states)
+        hidden_states = weights.img_in.apply(hidden_states.squeeze(0))
 
-        encoder_hidden_states = encoder_hidden_states.squeeze(0)
-
-        encoder_hidden_states = weights.txt_norm.apply(encoder_hidden_states)
+        encoder_hidden_states = weights.txt_norm.apply(encoder_hidden_states.squeeze(0))
         encoder_hidden_states = weights.txt_in.apply(encoder_hidden_states)
 
-        embed = weights.time_text_embed_timestep_embedder_linear_1.apply(self.scheduler.timesteps_proj)
-        embed0 = torch.nn.functional.silu(embed)
+        embed0 = weights.time_text_embed_timestep_embedder_linear_1.apply(self.scheduler.timesteps_proj)
+        embed0 = torch.nn.functional.silu(embed0)
         embed0 = weights.time_text_embed_timestep_embedder_linear_2.apply(embed0)
 
         if self.scheduler.infer_condition:
