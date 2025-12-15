d53baae7160cdb44597ec094f80e377c45da9692
helloyongyang <yongyang1030@163.com>
2025-12-15 20:34:43 +0000

    update configs

--- scripts/qwen_image/qwen_image_i2i_2511.sh
+++ scripts/qwen_image/qwen_image_i2i_2511.sh
@@ -13,9 +13,9 @@ python -m lightx2v.infer \
     --model_cls qwen_image \
     --task i2i \
     --model_path $model_path \
-    --config_json ${lightx2v_path}/configs/qwen_image/qwen_image_i2i_preview_2511.json \
+    --config_json ${lightx2v_path}/configs/qwen_image/qwen_image_i2i_2511.json \
     --prompt "Make the girl from Image 1 wear the black dress from Image 2 and sit in the pose from Image 3." \
     --negative_prompt " " \
     --image_path "1.png,2.png,3.png" \
-    --save_result_path ${lightx2v_path}/save_results/qwen_image_i2i_preview_2511.png \
+    --save_result_path ${lightx2v_path}/save_results/qwen_image_i2i_2511.png \
     --seed 0
