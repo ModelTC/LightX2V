97a38e04666adaeaad46e71d8d95924498e7732f
helloyongyang <yongyang1030@163.com>
2025-12-17 17:32:38 +0000

    1

--- lightx2v/common/ops/norm/triton_ops.py
+++ lightx2v/common/ops/norm/triton_ops.py
@@ -124,8 +124,10 @@ def fuse_scale_shift_kernel(
     block_l: int = 128,
     block_c: int = 128,
 ):
-    assert x.is_cuda and scale.is_cuda
+    # assert x.is_cuda and scale.is_cuda
     assert x.is_contiguous()
+    if x.dim() == 2:
+        x = x.unsqueeze(0)
 
     B, L, C = x.shape
     output = torch.empty_like(x)
@@ -826,7 +828,7 @@ def norm_infer(
     out: Optional[Tensor] = None,
 ):
     M, N = x.shape
-    assert x.stride(-1) == 1
+    x = x.contiguous()
     if weight is not None:
         assert weight.shape == (N,)
         assert weight.stride(-1) == 1
