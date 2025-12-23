a8d093d7818d44ee4e8963a88ba3f8375451fba8
Kane <1904007277@qq.com>
2025-12-23 08:04:54 +0000

    fix audio ca module attn

--- lightx2v/models/networks/wan/infer/audio/transformer_infer.py
+++ lightx2v/models/networks/wan/infer/audio/transformer_infer.py
@@ -20,7 +20,6 @@ class WanAudioTransformerInfer(WanOffloadTransformerInfer):
         super().__init__(config)
         self.has_post_adapter = True
         self.phases_num = 4
-        self.attn_obj = ATTN_WEIGHT_REGISTER.get(self.config.get("self_attn_1_type"))()
 
     @torch.no_grad()
     def infer_post_adapter(self, phase, x, pre_infer_out):
@@ -83,6 +82,22 @@ class WanAudioTransformerInfer(WanOffloadTransformerInfer):
 
         cu_seqlens_q = torch.cat([q_lens.new_zeros([1]), q_lens]).cumsum(0, dtype=torch.int32).to(q.device, non_blocking=True)
         cu_seqlens_k = torch.cat([k_lens.new_zeros([1]), k_lens]).cumsum(0, dtype=torch.int32).to(q.device, non_blocking=True)
-        out = self.attn_obj.apply(q=q, k=k, v=v, cu_seqlens_q=cu_seqlens_q, cu_seqlens_kv=cu_seqlens_k, max_seqlen_q=max_seqlen_q, max_seqlen_kv=max_seqlen_k)
+        if "npu" in AI_DEVICE:
+            out = ATTN_WEIGHT_REGISTER.get("npu_flash_attn")().apply(q=q, k=k, v=v, cu_seqlens_q=cu_seqlens_q, cu_seqlens_kv=cu_seqlens_k, max_seqlen_q=max_seqlen_q, max_seqlen_kv=max_seqlen_k)
+        else:
+            out = flash_attn_varlen_func(
+                q=q,
+                k=k,
+                v=v,
+                cu_seqlens_q=cu_seqlens_q,
+                cu_seqlens_k=cu_seqlens_k,
+                max_seqlen_q=max_seqlen_q,
+                max_seqlen_k=max_seqlen_k,
+                dropout_p=0.0,
+                softmax_scale=None,
+                causal=False,
+                window_size=(-1, -1),
+                deterministic=False,
+            )
         out = out.view(-1, self.num_heads * self.head_dim)
         return phase.to_out.apply(out) * gate
