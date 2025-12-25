dec7f9b2c6b156821aa41bc76922e165ee0f1e10
helloyongyang <yongyang1030@163.com>
2025-12-25 07:14:40 +0000

    1

--- scripts/platforms/hygon_dcu/qwen_image_i2i_2511.sh
+++ scripts/platforms/hygon_dcu/qwen_image_i2i_2511.sh
@@ -5,7 +5,7 @@ lightx2v_path=
 model_path=
 
 export PLATFORM=hygon_dcu
-export CUDA_VISIBLE_DEVICES=0
+export HIP_VISIBLE_DEVICES=0
 
 # set environment variables
 source ${lightx2v_path}/scripts/base/base.sh
