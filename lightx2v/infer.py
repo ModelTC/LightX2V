8bd3b00597c8d847ad7e0a39f882102a18f94ca4
helloyongyang <yongyang1030@163.com>
2026-01-14 06:15:08 +0000

    update

--- lightx2v/infer.py
+++ lightx2v/infer.py
@@ -18,7 +18,7 @@ from lightx2v.models.runners.wan.wan_sf_runner import WanSFRunner  # noqa: F401
 from lightx2v.models.runners.wan.wan_vace_runner import WanVaceRunner  # noqa: F401
 from lightx2v.models.runners.z_image.z_image_runner import ZImageRunner  # noqa: F401
 from lightx2v.utils.envs import *
-from lightx2v.utils.input_info import set_input_info_from_args
+from lightx2v.utils.input_info import init_empty_input_info, update_input_info_from_dict
 from lightx2v.utils.profiler import *
 from lightx2v.utils.registry_factory import RUNNER_REGISTER
 from lightx2v.utils.set_config import print_config, set_config, set_parallel_config
@@ -129,6 +129,8 @@ def main():
 
     # set config
     config = set_config(args)
+    # init input_info
+    input_info = init_empty_input_info(args.task)
 
     if config["parallel"]:
         platform_device = PLATFORM_DEVICE_REGISTER.get(os.getenv("PLATFORM", "cuda"), None)
@@ -138,8 +140,11 @@ def main():
     print_config(config)
 
     with ProfilingContext4DebugL1("Total Cost"):
+        # init runner
         runner = init_runner(config)
-        input_info = set_input_info_from_args(args)
+        # start to infer
+        data = args.__dict__
+        update_input_info_from_dict(input_info, data)
         runner.run_pipeline(input_info)
 
     # Clean up distributed process group
