a7d0fb3b6e3469c8fa08efb2e79e7c85eb92bdbc
root <root@pt-1566c00962444e589a1c9589088689e2-worker-0.pt-1566c00962444e589a1c9589088689e2.ns-devsft-3460edd0.svc.cluster.local>
2026-01-29 07:45:02 +0000

    update configs

--- scripts/wan22_moe_vace/run_wan22_moe_vace.sh
+++ scripts/wan22_moe_vace/run_wan22_moe_vace.sh
@@ -15,8 +15,7 @@ python -m lightx2v.infer \
 --task vace \
 --model_path $model_path \
 --config_json ${lightx2v_path}/configs/wan22_vace/a800/bf16/wan22_moe_vace.json \
-# --config_json ${lightx2v_path}/configs/wan22_vace/a800/int8/wan22_moe_vace.json \
---prompt "模仿视频的动作，翩翩起舞." \
+--prompt "图片的女人，穿着白色连衣裙，模仿视频的动作，翩翩起舞." \
 --negative_prompt "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走" \
 --src_video /path/to/post+depth.mp4 \
 --src_ref_images /path/to/image.png \
