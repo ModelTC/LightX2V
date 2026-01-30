8ffed6bf921080d08f74bb355627e05606762a4e
Yang Yong (雍洋) <yongyang1030@163.com>
2026-01-30 14:36:20 +0800

    update configs (#852)

--- scripts/qwen_image/qwen_image_i2i_2511_dist.sh
+++ scripts/qwen_image/qwen_image_i2i_2511_dist.sh
@@ -4,16 +4,16 @@
 export lightx2v_path=
 export model_path=
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # set environment variables
 source ${lightx2v_path}/scripts/base/base.sh
 
-python -m lightx2v.infer \
+torchrun --nproc_per_node=8 -m lightx2v.infer \
     --model_cls qwen_image \
     --task i2i \
     --model_path $model_path \
-    --config_json ${lightx2v_path}/configs/qwen_image/qwen_image_i2i_2511.json \
+    --config_json ${lightx2v_path}/configs/qwen_image/qwen_image_i2i_2511_dist.json \
     --prompt "Change the person to a standing position, bending over to hold the dog's front paws." \
     --negative_prompt " " \
     --image_path "/data/nvme1/yongyang/ddc/lightx2v_examples/i2i/1/img1.png" \
