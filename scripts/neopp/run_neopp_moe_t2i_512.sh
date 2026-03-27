b89f94dffd402d2d6a421d29effed9dc914c4b08
helloyongyang <yongyang1030@163.com>
2026-03-27 12:14:50 +0000

    update scripts

--- scripts/neopp/run_neopp_moe_t2i_512.sh
+++ scripts/neopp/run_neopp_moe_t2i_512.sh
@@ -4,7 +4,7 @@
 lightx2v_path=/data/nvme1/yongyang/FL/LightX2V
 model_path=/data/nvme1/yongyang/FL/neo_gen_30b_moe/neo_gen_30b_moe
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=5
 
 # set environment variables
 source ${lightx2v_path}/scripts/base/base.sh
@@ -14,8 +14,8 @@ python -m lightx2v.infer \
 --model_cls neopp \
 --task t2i \
 --model_path $model_path \
---config_json ${lightx2v_path}/configs/neopp/neopp_t2i.json \
+--config_json ${lightx2v_path}/configs/neopp/neopp_moe_t2i.json \
 --prompt "a photo of two trucks" \
---target_shape 1024 1024 \
+--target_shape 512 512 \
 --negative_prompt "" \
---save_result_path ${lightx2v_path}/save_results/output_lightx2v_neopp_t2i_1k.png
+--save_result_path ${lightx2v_path}/save_results/output_lightx2v_neopp_moe_t2i_512.png
