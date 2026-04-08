1ee7ccc5c7c012a441de60108b543be63f02dfa3
helloyongyang <yongyang1030@163.com>
2026-04-08 03:54:07 +0000

    fix 图文交错

--- examples/neopp/neopp_dense_1k.py
+++ examples/neopp/neopp_dense_1k.py
@@ -5,7 +5,7 @@ from lightx2v import LightX2VPipeline
 # -------------------------------------------------
 
 pipe = LightX2VPipeline(
-    model_path="/data/nvme1/yongyang/FL/neo9b/neo9b",
+    model_path="/data/nvme1/yongyang/FL/neo_9b_new/hf_step4000_ema",
     model_cls="neopp",
     support_tasks=["t2i", "i2i"],
 )
@@ -18,28 +18,72 @@ pipe.modify_config({"load_kv_cache_in_pipeline_for_debug": False})
 # Load KV cache and generate
 # -------------------------------------------------
 
-pipe.runner.load_kvcache_t2i(
-    "/data/nvme1/yongyang/FL/neo_test9b/vlm_tensor/to_x2v_cond_kv.pt",
-    "/data/nvme1/yongyang/FL/neo_test9b/vlm_tensor/to_x2v_uncond_kv.pt",
+# -------------------------------------------------
+# TURN 0
+# -------------------------------------------------
+pipe.runner.load_kvcache(
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_cond_kv_0_289.pt",
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_uncond_kv_0_9.pt",
+)
+pipe.runner.set_inference_params(
+    index_offset_cond=289,
+    index_offset_uncond=9,
+    cfg_interval=(-1, 2),
+    cfg_scale=4.0,
+    cfg_norm="global",
+    timestep_shift=3.0,
 )
 
 pipe.generate(
     seed=200,
-    task="t2i",
-    save_result_path="/data/nvme1/yongyang/FL/LightX2V/save_results/output_lightx2v_neopp_dense_t2i_1k.png",
+    save_result_path="/data/nvme1/yongyang/FL/LightX2V/save_results/output_lightx2v_neopp_dense_1k_0.png",
     target_shape=[1024, 1024],  # Height, Width
 )
 
 
-pipe.runner.load_kvcache_i2i(
-    "/data/nvme1/yongyang/FL/neo_test9b/vlm_tensor_it2i/to_x2v_cond_kv.pt",
-    "/data/nvme1/yongyang/FL/neo_test9b/vlm_tensor_it2i/to_x2v_uncond_kv_text.pt",
-    "/data/nvme1/yongyang/FL/neo_test9b/vlm_tensor_it2i/to_x2v_uncond_kv_img.pt",
+# -------------------------------------------------
+# TURN 1
+# -------------------------------------------------
+pipe.runner.load_kvcache(
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_cond_kv_1_346.pt",
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_uncond_kv_1_12.pt",
 )
+# If params are not set, the previous turn params will be used.
+# pipe.runner.set_inference_params(
+#     index_offset_cond=346,
+#     index_offset_uncond=12,
+#     cfg_interval=(-1, 2),
+#     cfg_scale=4.0,
+#     cfg_norm="global",
+#     timestep_shift=3.0,
+# )
 
 pipe.generate(
     seed=200,
-    task="i2i",
-    save_result_path="/data/nvme1/yongyang/FL/LightX2V/save_results/output_lightx2v_neopp_dense_i2i_1k.png",
+    save_result_path="/data/nvme1/yongyang/FL/LightX2V/save_results/output_lightx2v_neopp_dense_1k_1.png",
+    target_shape=[1024, 1024],  # Height, Width
+)
+
+
+# -------------------------------------------------
+# TURN 2
+# -------------------------------------------------
+pipe.runner.load_kvcache(
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_cond_kv_2_411.pt",
+    "/data/nvme1/yongyang/FL/neo_9b_new/vlm_tensor/to_x2v_uncond_kv_2_15.pt",
+)
+# If params are not set, the previous turn params will be used.
+# pipe.runner.set_inference_params(
+#     index_offset_cond=411,
+#     index_offset_uncond=15,
+#     cfg_interval=(-1, 2),
+#     cfg_scale=4.0,
+#     cfg_norm="global",
+#     timestep_shift=3.0,
+# )
+
+pipe.generate(
+    seed=200,
+    save_result_path="/data/nvme1/yongyang/FL/LightX2V/save_results/output_lightx2v_neopp_dense_1k_2.png",
     target_shape=[1024, 1024],  # Height, Width
 )
