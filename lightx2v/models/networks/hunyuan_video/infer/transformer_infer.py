c3bb83492b8fe800d7800e8b88785b030e967023
shihaobai <1798930569@qq.com>
2026-04-21 03:56:31 +0000

    fix as pre-commit

--- lightx2v/models/networks/hunyuan_video/infer/transformer_infer.py
+++ lightx2v/models/networks/hunyuan_video/infer/transformer_infer.py
@@ -10,7 +10,6 @@ except Exception as e:
     apply_rope_with_cos_sin_cache_inplace = None
 
 from lightx2v.common.transformer_infer.transformer_infer import BaseTransformerInfer
-from lightx2v_platform.base.global_var import AI_DEVICE
 
 from .module_io import HunyuanVideo15ImgBranchOutput, HunyuanVideo15TxtBranchOutput
 from .triton_ops import fuse_scale_shift_kernel
