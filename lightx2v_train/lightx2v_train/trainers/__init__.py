4128a3a2be47f1f01297adee8dc1102f5943dabb
helloyongyang <yongyang1030@163.com>
2026-05-11 03:14:28 +0000

    update

--- lightx2v_train/lightx2v_train/trainers/__init__.py
+++ lightx2v_train/lightx2v_train/trainers/__init__.py
@@ -1,8 +1,6 @@
 from lightx2v_train.utils.registry import TRAINER_REGISTER, build_trainer
 
-from .distill import DistillTrainer
 from .full_finetune import FullFinetuneTrainer
 from .lora import LoraTrainer
-from .rl import RLTrainer
 
-__all__ = ["DistillTrainer", "FullFinetuneTrainer", "LoraTrainer", "RLTrainer", "TRAINER_REGISTER", "build_trainer"]
+__all__ = ["FullFinetuneTrainer", "LoraTrainer", "TRAINER_REGISTER", "build_trainer"]
