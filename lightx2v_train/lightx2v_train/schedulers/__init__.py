ef053f4f6f2895d6bece6e55c2495ae7f7c23aeb
Musisoul <musisoul@qq.com>
2026-05-13 09:23:43 +0000

    refactor

--- lightx2v_train/lightx2v_train/schedulers/__init__.py
+++ lightx2v_train/lightx2v_train/schedulers/__init__.py
@@ -1,4 +1,4 @@
-from .dmd import DMDFlowMatchingScheduler
+from .dmd_scheduler import DMDFlowMatchingScheduler
 from .flow_matching import RectifiedFlowMatchingScheduler
 
 __all__ = ["DMDFlowMatchingScheduler", "RectifiedFlowMatchingScheduler"]
