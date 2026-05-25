8168ebceecfee8ebaa0545c92a15ea7b0f565dc4
Shiqiao Gu (谷石桥) <77222802+gushiqiao@users.noreply.github.com>
2026-05-25 15:55:24 +0800

    Delete lightx2v_kernel/test/kvcache/run_test_nvfp4_kv.sh

--- lightx2v_kernel/test/kvcache/run_test_nvfp4_kv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-lightx2v_path=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)
-
-export PYTHONPATH="${lightx2v_path}/lightx2v_kernel/python:${lightx2v_path}:${PYTHONPATH}"
-
-python "${lightx2v_path}/lightx2v_kernel/test/kvcache/test_nvfp4_kv.py"
