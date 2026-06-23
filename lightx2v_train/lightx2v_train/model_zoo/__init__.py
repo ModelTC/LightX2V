5ad329d9f7eb62a0e6b47c4a8d873f313adb0701
Musisoul <musisoul@qq.com>
2026-06-23 03:32:58 +0000

    lint

--- lightx2v_train/lightx2v_train/model_zoo/__init__.py
+++ lightx2v_train/lightx2v_train/model_zoo/__init__.py
@@ -1,7 +1,7 @@
 from lightx2v_train.utils.registry import build_model
 
-from .flux2_klein import Flux2KleinModel
 from .flux2_dev import Flux2DevModel
+from .flux2_klein import Flux2KleinModel
 from .longcat_image import LongCatImageModel
 from .qwen_image import QwenImageModel
 from .wan_t2v import WanT2VModel
