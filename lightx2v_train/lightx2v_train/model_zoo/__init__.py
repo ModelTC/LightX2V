d0f899d2e092f502d26fd59072e695ccd90f8eb5
gushiqiao <975033167@qq.com>
2026-07-02 09:05:08 +0000

    support wan22 5b, wan21 14b train

--- lightx2v_train/lightx2v_train/model_zoo/__init__.py
+++ lightx2v_train/lightx2v_train/model_zoo/__init__.py
@@ -4,5 +4,6 @@ from .flux2_klein import Flux2KleinModel
 from .longcat_image import LongCatImageModel
 from .qwen_image import QwenImageModel
 from .wan_t2v import WanT2VModel
+from .wan_ti2v_5b import WanTI2V5BModel
 
-__all__ = ["build_model", "QwenImageModel", "LongCatImageModel", "Flux2KleinModel", "WanT2VModel"]
+__all__ = ["build_model", "QwenImageModel", "LongCatImageModel", "Flux2KleinModel", "WanT2VModel", "WanTI2V5BModel"]
