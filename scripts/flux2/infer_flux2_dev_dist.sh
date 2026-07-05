bbf89ba8efd9c0038bd4eec2f57fee6e38b81c11
Watebear <wushuo@bupt.cn>
2026-07-05 17:41:07 +0800

    fix

--- scripts/flux2/infer_flux2_dev_dist.sh
+++ scripts/flux2/infer_flux2_dev_dist.sh
@@ -11,4 +11,4 @@ torchrun --nproc_per_node=4 -m lightx2v.infer \
     --model_path $model_path \
     --prompt "Realistic macro photograph of a hermit crab using a soda can as its shell, partially emerging from the can, captured with sharp detail and natural colors, on a sunlit beach with soft shadows and a shallow depth of field, with blurred ocean waves in the background. The can has the text `BFL Diffusers` on it and it has a color gradient that start with #FF5733 at the top and transitions to #33FF57 at the bottom." \
     --save_result_path "${lightx2v_path}/save_results/flux2_dev.png" \
-    --config_json "${lightx2v_path}/configs/flux2/flux2_dev_dist.json"
+    --config_json "${lightx2v_path}/configs/flux2/flux2_dev_tp.json"
