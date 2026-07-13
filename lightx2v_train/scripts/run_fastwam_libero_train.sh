ca2d5840b6c75198b7e7e07e0d0cd3efda2ce545
Musisoul <musisoul@qq.com>
2026-07-13 08:41:00 +0000

    u

--- lightx2v_train/scripts/run_fastwam_libero_train.sh
+++ lightx2v_train/scripts/run_fastwam_libero_train.sh
@@ -5,7 +5,6 @@ LIGHTX2V_ROOT="${LIGHTX2V_ROOT:-/data/nvme0/chendingyu/LightX2V}"
 CONFIG_PATH="${CONFIG_PATH:-${LIGHTX2V_ROOT}/lightx2v_train/configs/train/fastwam/libero_uncond_2cam224.yaml}"
 
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-1}"
-export PYTHONPATH="${LIGHTX2V_ROOT}/lightx2v_train:${LIGHTX2V_ROOT}:${PYTHONPATH:-}"
 
 cd "${LIGHTX2V_ROOT}"
 python lightx2v_train/train.py --config "${CONFIG_PATH}" "$@"
