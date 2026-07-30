e3c1f38c621ded6472340f361ebbc3f07b06f8b8
Yang <michael20070814@outlook.com>
2026-07-30 04:20:52 -0500

    repair some import problem which is caused by project version

--- lightx2v/models/networks/wan/weights/transformer_weights.py
+++ lightx2v/models/networks/wan/weights/transformer_weights.py
@@ -2,7 +2,6 @@ import torch
 import torch.distributed as dist
 
 from lightx2v.common.modules.weight_module import WeightModule, WeightModuleList
-from lightx2v.models.networks.wan.infer.utils import WanCausalRope  # noqa: F401
 from lightx2v.utils.registry_factory import (
     ATTN_WEIGHT_REGISTER,
     LN_WEIGHT_REGISTER,
