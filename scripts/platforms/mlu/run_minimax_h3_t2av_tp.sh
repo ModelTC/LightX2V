e913bfcf5893009104bd9add1da406f562c04564
root <root@dev-mlu-0818-0.dev-mlu-0818.feeba6bf-3c9a-4551-8c89-7df48b33dcd5.svc.cluster.local>
2026-08-20 19:42:26 +0800

    update

--- scripts/platforms/mlu/run_minimax_h3_t2av_tp.sh
+++ scripts/platforms/mlu/run_minimax_h3_t2av_tp.sh
@@ -22,8 +22,8 @@ nohup torchrun --standalone --nproc_per_node=8 -m lightx2v.infer \
     --model_cls minimax_h3 \
     --task t2av \
     --model_path $model_path \
-    --config_json ${lightx2v_path}/configs/platforms/mlu/minimax_h3_t2av_tp_sp.json \
+    --config_json ${lightx2v_path}/configs/platforms/mlu/minimax_h3_t2av_tp.json \
     --prompt "$prompt" \
     --save_result_path ${lightx2v_path}/save_results/output_lightx2v_minimax_h3_t2av.mp4 \
     --seed 0 \
-    --warmup > ${lightx2v_path}/save_results/minimax_h3_t2av_544p_124_8gpu_tp4_sp2_comile_torch_real.log 2>&1 &
+    --warmup > ${lightx2v_path}/save_results/minimax_h3_t2av_544p_124_8gpu_tp8_comile_torch_real_rope.log 2>&1 &
