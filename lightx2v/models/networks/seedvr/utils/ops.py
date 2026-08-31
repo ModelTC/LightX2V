ec8582ccf72f9756ae3c561075ca5ce1dbbc0e51
gaclove <peng.gaoc@gmail.com>
2026-08-31 18:47:47 +0800

    fix(seedvr): restore distributed ops exports

--- lightx2v/models/networks/seedvr/utils/ops.py
+++ lightx2v/models/networks/seedvr/utils/ops.py
@@ -1,5 +1,12 @@
 import torch.nn.functional as F
 
+from lightx2v.models.video_encoders.hf.seedvr.common.distributed.ops import (
+    gather_heads_scatter_seq,
+    gather_outputs,
+    gather_seq_scatter_heads_qkv,
+    slice_inputs,
+)
+
 
 def safe_pad_operation(x, pad):
     return F.pad(x, pad)
