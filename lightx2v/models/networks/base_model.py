a8a813b7a954193d58e1b1fb79d5173c72f6ef6e
zhangbilang <2045955563@qq.com>
2026-08-31 04:11:25 +0000

    fix(hunyuan): correct SR transformer weight loading and delete run_step()

--- lightx2v/models/networks/base_model.py
+++ lightx2v/models/networks/base_model.py
@@ -440,10 +440,7 @@ class BaseTransformerModel(ABC):
         if _tp_dev:
             _saved_device, self.device = self.device, torch.device("cpu")
 
-        if self.config.get("dit_original_ckpt", None):
-            safetensors_path = self.config["dit_original_ckpt"]
-        else:
-            safetensors_path = self.model_path
+        safetensors_path = self.config.get("dit_original_ckpt") or self.config.get("transformer_model_path") or self.model_path
 
         if os.path.isdir(safetensors_path):
             if self.lazy_load:
@@ -454,10 +451,7 @@ class BaseTransformerModel(ABC):
                 else:
                     raise ValueError(f"Non-block file not found in {safetensors_path}. Please check the model path.")
             else:
-                if self.config["model_cls"] == "hunyuan_video_1.5":
-                    safetensors_files = glob.glob(os.path.join(safetensors_path, "transformer", self.config["transformer_model_name"], "*.safetensors"))
-                else:
-                    safetensors_files = glob.glob(os.path.join(safetensors_path, "*.safetensors"))
+                safetensors_files = glob.glob(os.path.join(safetensors_path, "*.safetensors"))
         else:
             if self.lazy_load:
                 self.lazy_load_path = safetensors_path
