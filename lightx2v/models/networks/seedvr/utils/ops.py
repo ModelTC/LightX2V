a5c20c84a4f7385c78ba55eb8d5329323fb0412a
gaclove <peng.gaoc@gmail.com>
2026-08-31 18:55:03 +0800

    style(seedvr): make compatibility aliases explicit

--- lightx2v/models/networks/seedvr/utils/ops.py
+++ lightx2v/models/networks/seedvr/utils/ops.py
@@ -1,11 +1,11 @@
 import torch.nn.functional as F
 
-from lightx2v.models.video_encoders.hf.seedvr.common.distributed.ops import (
-    gather_heads_scatter_seq,
-    gather_outputs,
-    gather_seq_scatter_heads_qkv,
-    slice_inputs,
-)
+from lightx2v.models.video_encoders.hf.seedvr.common.distributed import ops as distributed_ops
+
+gather_heads_scatter_seq = distributed_ops.gather_heads_scatter_seq
+gather_outputs = distributed_ops.gather_outputs
+gather_seq_scatter_heads_qkv = distributed_ops.gather_seq_scatter_heads_qkv
+slice_inputs = distributed_ops.slice_inputs
 
 
 def safe_pad_operation(x, pad):
